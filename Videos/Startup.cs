472c3697f587e8cec2a86d90620d0f82a078e196
fdinunzio@athirat.com <fdinunzio@athirat.com>
2019-10-01 17:18:09 +0200

    Aggiunta "Videos" (lezione su Pluralsight su Introduzione a MVC)

---
+++ Videos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Videos.Startup))]
+namespace Videos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
